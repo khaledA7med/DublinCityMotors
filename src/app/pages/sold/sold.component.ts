b95b6596df3db66bdfda1e9b9b9d1797ad1615c6
Khalid Ahmed Ahmed <khalid.ahmed@oasisoft.net>
2024-09-18 10:32:21 +0300

    []

--- src/app/pages/sold/sold.component.ts
+++ src/app/pages/sold/sold.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-sold',
   templateUrl: './sold.component.html',
-  styleUrls: ['./sold.component.scss']
+  styleUrls: ['./sold.component.scss'],
 })
 export class SoldComponent implements OnInit {
-  constructor(){}
-  ngOnInit(): void {}
+  constructor(private spinner: NgxSpinnerService) {}
+  ngOnInit(): void {
+    this.spinner.show();
 
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 3000);
+  }
 }
